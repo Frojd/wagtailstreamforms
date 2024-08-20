cfa69420589a8c2e12b0e7943705cc87a3bfba8e
vctrklndr <victor@nasa.se>
2024-08-20 22:01:48 +0200

    Add requirements

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ from wagtailstreamforms import __version__
 
 tests_require = [
     "mock==4.0.2",
+    "pytest-django==4.7.0",
+    "pytest==7.3.2",
     # Linting
     "isort[pyproject]==4.3.21",
     "flake8==3.7.9",
