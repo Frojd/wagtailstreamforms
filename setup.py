09ee4763933dff69ef401d7572e821c792803404
vctrklndr <victor@nasa.se>
2025-09-17 16:33:57 +0200

    Update generic chooser required version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ install_requires = [
     "wagtail-modeladmin",
     "wagtail>=5.2,<7.2",
     "Unidecode>=0.04.14,<2.0",
-    "wagtail-generic-chooser>=0.5.0,<0.7",
+    "wagtail-generic-chooser>=0.5.0,<0.8",
 ],
 
 documentation_extras = [
